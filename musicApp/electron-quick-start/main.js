f2dea796f41060712bc4159d4e2733ca4247fda0
Jarvis <598494198@qq.com>
2020-01-30 23:10:12 +0800

    restart

---
+++ musicApp/electron-quick-start/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+class AppWindow extends BrowserWindow {
+  constructor(config, fileLocation) {
+    const basicConfig = {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      } 
+    }
+    const finalConfig = { ...basicConfig, ...config }
+    super(finalConfig)
+    this.loadFile(fileLocation)
+    this.once('ready-to-show', ()=>{
+      this.show()
+    })
+  }
+
+}
+app.on('ready', ()=>{
+  const mainWindow = new AppWindow({}, './renderer/index.html')
+
+  ipcMain.on('add-music-window', ()=>{
+    const addWindow = new AppWindow({
+      width: 600,
+      height: 400,
+      parent: mainWindow
+    }, './renderer/add.html')
+  })
+})
